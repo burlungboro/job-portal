109c6a4eecf1502cbd4e04d1baa639d4e6c7643e
Burlung Boro <burlungboro@gmail.com>
2026-09-13 22:20:39 +0530

    Add my applications controller

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -64,6 +64,43 @@ const applyForJob = async (req, res) => {
 	}
 };
 
+const getMyApplications = async (req, res) => {
+	const candidate_id = req.user.id;
+
+	try {
+		const [applications] = await db.execute(
+			`SELECT
+				ja.job_id,
+				j.title AS job_title,
+				j.location AS job_location,
+				j.employment_type,
+				j.company_id,
+				ja.status,
+				ja.cover_letter,
+				ja.applied_at,
+				ja.updated_at,
+				c.name AS company_name
+			 FROM job_applications ja
+			 JOIN jobs j ON ja.job_id = j.id
+			 JOIN companies c ON j.company_id = c.id
+			 WHERE ja.candidate_id = ?
+			 ORDER BY ja.applied_at DESC`,
+			[candidate_id]
+		);
+
+		return res.status(200).json({
+			applications,
+		});
+	} catch (error) {
+		console.error("Get my applications error:", error);
+
+		return res.status(500).json({
+			message: "Server error while retrieving applications",
+		});
+	}
+};
+
 module.exports = {
 	applyForJob,
+	getMyApplications,
 };
