05471e9bbc5a5db299104a4b6e4f1114c192eedb
Burlung Boro <burlungboro@gmail.com>
2026-09-13 22:39:32 +0530

    Add recruiter applications route

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,22 +1,35 @@
 const express = require("express");
+
 const { authMiddleware } = require("../middleware/authMiddleware");
 const { requireRole } = require("../middleware/roleMiddleware");
-const { applyForJob, getMyApplications } = require("../controllers/applicationController");
+
+const {
+    applyForJob,
+    getMyApplications,
+    getApplicationsForRecruiter,
+} = require("../controllers/applicationController");
 
 const router = express.Router();
 
 router.get(
-	"/",
-	authMiddleware,
-	requireRole("CANDIDATE"),
-	getMyApplications
+    "/",
+    authMiddleware,
+    requireRole("CANDIDATE"),
+    getMyApplications
 );
 
 router.post(
-	"/",
-	authMiddleware,
-	requireRole("CANDIDATE"),
-	applyForJob
+    "/",
+    authMiddleware,
+    requireRole("CANDIDATE"),
+    applyForJob
+);
+
+router.get(
+    "/recruiter",
+    authMiddleware,
+    requireRole("RECRUITER"),
+    getApplicationsForRecruiter
 );
 
 module.exports = router;
